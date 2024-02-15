4d1bd65f93f89530ed7e02bed2b37819527a64e3
1Ashutosh1 <at8969927@gmail.com>
2024-02-15 16:36:55 +0530

    Updated JS file

--- script.js
+++ script.js
@@ -1,72 +1,51 @@
-
-let randomNumber = parseInt(Math.random() * 100 + 1);
-
-const submit = document.querySelector('#subt');
-const userInput = document.querySelector('#guessField');
-const guessSlot = document.querySelector('.guesses');
-const remaining = document.querySelector('.lastResult');
+let randomNumber = Math.floor(Math.random()*100 + 1);
+let prevGuesses = [];
+const guesses = document.querySelector('.guesses');
 const lowOrHi = document.querySelector('.lowOrHi');
+let remaining = 10;
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('.guessField');
+const GuessesRemaining = document.querySelector('.lastResult');
 const startOver = document.querySelector('.resultParas');
 
 const p = document.createElement('p');
-
-let prevGuess = [];
-let numGuess = 1;
-
 let playGame = true;
 
-if (playGame) {
-  submit.addEventListener('click', function (e) {
-    e.preventDefault();
-    const guess = parseInt(userInput.value);
-    console.log(guess);
-    validateGuess(guess);
-  });
-}
-
-function validateGuess(guess) {
-  if (isNaN(guess)) {
-    alert('PLease enter a valid number');
-  } else if (guess < 1) {
-    alert('PLease enter a number more than 1');
-  } else if (guess > 100) {
-    alert('PLease enter a  number less than 100');
-  } else {
-    prevGuess.push(guess);
-    if (numGuess === 11) {
-      displayGuess(guess);
-      displayMessage(`Game Over. Random number was ${randomNumber}`);
-      endGame();
-    } else {
-      displayGuess(guess);
-      checkGuess(guess);
-    }
+function checkValid(){
+  const num = parseInt(userInput.value);
+  if(num == '' || num < 1 || num > 100 || isNaN(num)){
+    lowOrHi.innerHTML = "<h2>Enter a Valid Number</h2>";
+  } else{
+    compareNum(num);
   }
 }
 
-function checkGuess(guess) {
-  if (guess === randomNumber) {
-    displayMessage(`You guessed it right`);
+function compareNum(num){
+  if(remaining > 0){
+   if(num > randomNumber){
+     lowOrHi.innerHTML = `<h2>The Number is Lower than ${num}</h2>`;
+     prevGuesses.push(num);
+     guesses.innerHTML = prevGuesses;
+     remaining--;
+     GuessesRemaining.innerHTML = remaining;
+   } else if(num < randomNumber){
+      lowOrHi.innerHTML = `<h2>The Number is Higher than ${num}</h2>`;
+      prevGuesses.push(num);
+      guesses.innerHTML = prevGuesses;
+      remaining--;
+      GuessesRemaining.innerHTML = remaining;
+   } else {
+    lowOrHi.innerHTML = "<h2>You guessed the number correctly</h2>";
     endGame();
-  } else if (guess < randomNumber) {
-    displayMessage(`Number is TOOO low`);
-  } else if (guess > randomNumber) {
-    displayMessage(`Number is TOOO High`);
+   } 
+  }
+  else{
+    lowOrHi.innerHTML = `<h2>Game Over. The Random Number was ${randomNumber}`;
+    endGame();
   }
 }
 
-function displayGuess(guess) {
-  userInput.value = '';
-  guessSlot.innerHTML += `${guess}, `;
-  numGuess++;
-  remaining.innerHTML = `${11 - numGuess} `;
-}
-
-function displayMessage(message) {
-  lowOrHi.innerHTML = `<h2>${message}</h2>`;
-}
-
-function endGame() {
+function endGame(){
   userInput.value = '';
   userInput.setAttribute('disabled', '');
   p.classList.add('button');
@@ -76,18 +55,24 @@ function endGame() {
   newGame();
 }
 
-function newGame() {
-  const newGameButton = document.querySelector('#newGame');
-  newGameButton.addEventListener('click', function (e) {
-    randomNumber = parseInt(Math.random() * 100 + 1);
-    prevGuess = [];
-    numGuess = 1;
-    guessSlot.innerHTML = '';
-    remaining.innerHTML = `${11 - numGuess} `;
+function newGame(){
+  const startButton = document.querySelector('#newGame');
+  startButton.addEventListener('click', function(e){
+    randomNumber = Math.floor(Math.random()*100 + 1);
+    prevGuesses = [];
+    remaining = 10;
+    guesses.innerHTML = '';
+    GuessesRemaining.innerHTML = remaining;
+    lowOrHi.innerHTML = '';
     userInput.removeAttribute('disabled');
     startOver.removeChild(p);
-
     playGame = true;
   });
 }
 
+if(playGame){
+  submit.addEventListener('click',function(e){
+    e.preventDefault();
+    checkValid();
+  });
+}
